6da8296764a32df29b8ac18ed4f2a6510a7219d2
asifdahir <m.asif.4849@gmail.com>
2015-12-17 15:54:37 +0500

    - mac

--- app/src/main/java/com/home/intelligentsystems/cardreader/Common.java
+++ app/src/main/java/com/home/intelligentsystems/cardreader/Common.java
@@ -8,7 +8,8 @@ public class Common {
 
     public static final String KEY_CARD_CONTENT = "KEY_CARD_CONTENT";
     public static final String KEY_EMPLOYEE = "KEY_EMPLOYEE";
-    public static final String SERVICE_URL = "http://192.168.2.126:4321/api/Employee/";
+    //public static final String SERVICE_URL = "http://192.168.2.126:4321/api/Employee/";
+    public static final String SERVICE_URL = "http://192.168.2.126:4321/api/Employee?cnic=";
     public static final int SERVICE_PORT = 8080;
 }
 
